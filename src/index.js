a8b297cea93756c904920b6e7e3ca04aae2a3bf8
July Terekhova <sky206380@gmail.com>
2023-06-20 13:05:28 +0300

    Solution 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import './App.scss'
 import ScrollToTop from "./ScrollToTop";
@@ -9,12 +9,12 @@ import {AppProvider} from "./helpers/appContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.Fragment>
-        <BrowserRouter>
+        <Router>
         <ScrollToTop />
             <AppProvider>
                 <App />
             </AppProvider>
-        </BrowserRouter>
+        </Router>
     </React.Fragment>
 );
 
